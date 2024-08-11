d6e40c6c344bc881a4bb8c2abcc5547be5b92a07
ibrahem abodeif <116855893+ibrahem-mohamedabodeif@users.noreply.github.com>
2024-08-11 05:13:37 +0300

    initial commit

--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -3,37 +3,29 @@ import { getReservations } from "@/lib/functions";
 import { createServerComponentClient } from "@/lib/server";
 import { redirect } from "next/navigation";
 
-// type reservationRoom = {
-//   id: string;
-//   totalPrice: number;
-//   nights: number;
-//   startDay: string;
-//   endDay: string;
-//   roomId: string;
-//   rooms: {
-//     city: string;
-//     image1: string;
-//     country: string;
-//     roomName: string;
-//     hostedName: string;
-//   };
-// };
+type RoomDetails = {
+  city: string;
+  image1: string;
+  country: string;
+  roomName: string;
+  hostedName: string;
+};
 
-type Room = {
-  id: any;
-  totalPrice?: number;
-  nights?: number;
-  startDay?: string;
-  endDay?: string;
-  roomId?: string;
-  rooms?: {
+interface Room {
+  id: string;
+  totalPrice: number;
+  nights: number;
+  startDay: string;
+  endDay: string;
+  roomId: string;
+  rooms: {
     city: string;
     image1: string;
     country: string;
     roomName: string;
     hostedName: string;
-  };
-};
+  }[];
+}
 
 export default async function Page() {
   const supabase = createServerComponentClient();
@@ -43,17 +35,17 @@ export default async function Page() {
 
   if (!user) redirect("/signin");
 
-  const reservationRooms: any = await getReservations(user.id);
+  const reservationRooms: Room[] | null = await getReservations(user.id);
 
   return (
     <>
-      {!reservationRooms.length && (
+      {!reservationRooms?.length && (
         <div className="text-4xl flex justify-center mt-40">
-          No reservatios yet
+          No reservations yet
         </div>
       )}
       <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-y-10 gap-16 mb-24">
-        {reservationRooms?.map((room: Room) => (
+        {reservationRooms?.map((room) => (
           <ReservationCard room={room} key={room.id} />
         ))}
       </div>
