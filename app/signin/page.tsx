d186c7624a3b5e28b35dee98d9cadeff711285a5
ibrahem abodeif <116855893+ibrahem-mohamedabodeif@users.noreply.github.com>
2024-08-11 10:39:13 +0300

    initial commit

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,15 +1,9 @@
-"use client";
 import logo from "@/app/icon.ico";
-import { signIn } from "@/lib/actions";
+import SigninForm from "@/components/signinForm";
 import Image from "next/image";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  const signInWithSearchQuery = signIn.bind(null, searchParams);
-
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -20,70 +14,9 @@ export default function Page() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action={signInWithSearchQuery} className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                autoComplete="current-password"
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-10 text-center text-md text-gray-500">
-          Not a member?{" "}
-          <Link
-            href={
-              searchParams
-                ? `/signup?${new URLSearchParams(searchParams).toString()}`
-                : "/signup"
-            }
-            className="text-lg font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Sign Up
-          </Link>
-        </p>
+        <Suspense fallback={<div>Loading form...</div>}>
+          <SigninForm />
+        </Suspense>
       </div>
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-
 import logo from "@/app/icon.ico";
-import { signUp } from "@/lib/actions";
+import SignupForm from "@/components/signupForm";
 import Image from "next/image";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  const signUpWithSearchQuery = signUp.bind(null, searchParams);
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -20,91 +14,9 @@ export default function Page() {
       </div>
 
       <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action={signUpWithSearchQuery} className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              Full Name
-            </label>
-            <div className="mt-2">
-              <input
-                name="fullName"
-                type="text"
-                required
-                autoComplete="name"
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              Phone Number
-            </label>
-            <div className="mt-2">
-              <input
-                name="phoneNumber"
-                type="text"
-                required
-                autoComplete="phoneNumber"
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex ">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Password
-              </label>
-            </div>
-            <div className="mt-2 mb-7">
-              <input
-                name="password"
-                type="password"
-                required
-                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign up
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-10 text-center text-md text-gray-500">
-          Have an account?{" "}
-          <Link
-            href={
-              searchParams
-                ? `/signin?${new URLSearchParams(searchParams).toString()}`
-                : "/signin"
-            }
-            className="text-lg font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Sign In
-          </Link>
-        </p>
+        <Suspense fallback={<div>Loading form...</div>}>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
