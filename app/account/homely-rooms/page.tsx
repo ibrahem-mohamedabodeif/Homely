3281f94e23b664378df040ede4e9d64567ae1e5f
ibrahem abodeif <ibrahem.mohamed191999@gmail.com>
2024-12-30 10:31:29 +0200

    edit hoomly room page

--- app/account/homely-rooms/page.tsx
+++ app/account/homely-rooms/page.tsx
@@ -10,7 +10,7 @@ const user = await currentUser()
       {!homelyRooms?.length ? (
         <div className="text-4xl flex justify-center mt-40">No Rooms yet</div>
       ) : (
-        <div className="grid grid-cols-1 items-center w-full mx-auto md:grid-cols-2 justify-center gap-y-10 gap-16 mb-24">
+        <div className="grid grid-cols-1 items-center w-full mx-auto md:grid-cols-3 justify-center gap-y-10 gap-16 mb-24">
           {homelyRooms.map((room) => (
             <HomelyRoomCard room={room} key={room.id} />
           ))}
